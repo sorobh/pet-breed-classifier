bff45a833616a01c6de3a259c60a4add01031739
sorobh <reachsaurabhnayak@gmail.com>
2023-06-19 17:10:48 -0700

    Update

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 import gradio as gr
+from fastai.vision.all import *
+import skimage
 
-def greet(name):
-    return "Hello " + name + "!!"
+learn = load_learner('export.pkl')
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+labels = learn.dls.vocab
+def predict(img):
+    img = PILImage.create(img)
+    pred,pred_idx,probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+title = "Pet Breed Classifier"
+description = "A pet breed classifier trained on the Oxford Pets dataset with fastai. Created as a demo for Gradio and HuggingFace Spaces."
+article="<p style='text-align: center'><a href='https://tmabraham.github.io/blog/gradio_hf_spaces_tutorial' target='_blank'>Blog post</a></p>"
+examples = []
+interpretation='default'
+enable_queue=True
+
+gr.Interface(fn=predict,
+             inputs=gr.inputs.Image(label="Upload a jpg image",type="filepath"),
+             outputs=gr.outputs.Label(num_top_classes=3),
+             title=title,
+            ).launch()
